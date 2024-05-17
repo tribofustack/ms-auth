fec1677a9e8858c039d8a1b746c20d83345bca90
deal-machine <95deal@gmail.com>
2024-05-17 00:16:58 -0300

    feature: cloud arch

--- index.ts
+++ index.ts
@@ -8,13 +8,13 @@ dotenv.config({
 function normalizePort(val: string | number): number {
   let port = parseInt(typeof val === "string" ? val : val.toString(), 10);
   if (isNaN(port)) {
-    return 3001;
+    return 3000;
   } else {
     return port;
   }
 }
 
-const port: number = normalizePort(process.env.PORT || 3001);
+const port: number = normalizePort(process.env.PORT || 3000);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
